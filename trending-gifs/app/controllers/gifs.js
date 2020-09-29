d719794b6265787df8f5847a0c243a75bfdca53f
Matt Northmore <rmnorthmore@gmail.com>
2020-09-29 15:01:27 -0400

    Adding feature to filter gifs

--- trending-gifs/app/controllers/gifs.js
+++ trending-gifs/app/controllers/gifs.js
@@ -1,4 +1,19 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import { action } from "@ember/object";
 
 export default class GifsController extends Controller {
+  gifData = [];
+  filteredGifData = [];
+
+
+  @action
+  searchTrendingGifs(event) {
+    this.filteredGifData = []
+    this.model.forEach((gif) => {
+      if (gif.title.includes(event.target.value)) {
+        this.filteredGifData.push(gif);
+      }
+    });
+    console.log(this.filteredGifData);
+  }
 }
--- trending-gifs/app/routes/gifs.js
+++ trending-gifs/app/routes/gifs.js
@@ -8,10 +8,15 @@ export default class GifsRoute extends Route {
     
     async model() {
         // Returns a promise from the giphy SDK and 
-        var fetchGifs = await gf.trending({ offset: 0, limit: 25 }).then((response) => {
+        var fetchGifs = await gf.trending({ offset: 0, limit: 50 }).then((response) => {
             console.log(response.data)
             return response.data;
         })
         return fetchGifs;
+    }
+
+    setupController(controller, model) {
+        super.setupController(controller, model)
+        controller.filteredGifData = model
     }
 }
